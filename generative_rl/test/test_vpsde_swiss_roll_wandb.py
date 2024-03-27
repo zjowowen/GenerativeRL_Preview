ddc6b17667a5d5831e66df06a6c9afbcbcb66914
thedreamfish <rongkunxue@outlook.com>
2024-03-27 18:05:05 +0800

    add the style format

--- generative_rl/test/test_vpsde_swiss_roll_wandb.py
+++ generative_rl/test/test_vpsde_swiss_roll_wandb.py
@@ -7,6 +7,7 @@ from rich.progress import track
 import numpy as np
 from sklearn.datasets import make_swiss_roll
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -19,36 +20,36 @@ from generative_rl.utils.log import log
 from generative_rl.utils.config import merge_two_dicts_into_newone
 
 sweep_config = EasyDict(
-    name = "base-sweep",
-    metric = dict(
-        name = "loss",
-        goal = "minimize",
+    name="base-sweep",
+    metric=dict(
+        name="loss",
+        goal="minimize",
     ),
-    method = "grid",
-    parameters = dict(
-        diffusion_model = dict(
-            parameters = dict(
-                path = dict(
-                    parameters = dict(
-                        type = dict(
-                            values = ["linear_vp_sde","linear","gvp"],
-                        ),
+    method="grid",
+    parameters=dict(
+        diffusion_model=dict(
+            parameters=dict(
+                path=dict(
+                    parameters=dict(
+                        type=dict(values=["linear_vp_sde", "linear", "gvp"], ),
                     ),
                 ),
-                model = dict(
-                    parameters = dict(
-                        type = dict(
-                            values = ["velocity_function", "score_function", "noise_function"],
+                model=dict(
+                    parameters=dict(
+                        type=dict(
+                            values=[
+                                "velocity_function", "score_function",
+                                "noise_function"
+                            ],
                         ),
                     ),
-                
                 ),
             ),
         ),
-        parameter = dict(
-            parameters = dict(
-                training_loss_type = dict(
-                    values = ["flow_matching", "score_matching"],
+        parameter=dict(
+            parameters=dict(
+                training_loss_type=dict(
+                    values=["flow_matching", "score_matching"],
                 ),
             ),
         ),
@@ -56,55 +57,52 @@ sweep_config = EasyDict(
 )
 
 
-
 def train_func():
-    x_size=2
-    device = torch.device('cuda:3') if torch.cuda.is_available() else torch.device('cpu')
+    x_size = 2
+    device = torch.device('cuda:3') if torch.cuda.is_available(
+    ) else torch.device('cpu')
     t_embedding_dim = 32
     t_encoder = dict(
-        type = "GaussianFourierProjectionTimeEncoder",
-        args = dict(
-            embed_dim = t_embedding_dim,
-            scale = 30.0,
+        type="GaussianFourierProjectionTimeEncoder",
+        args=dict(
+            embed_dim=t_embedding_dim,
+            scale=30.0,
         ),
     )
 
-
     origin_config = EasyDict(
         dict(
-            device = device,
-            diffusion_model = dict(
-                device = device,
-                x_size = x_size,
-                alpha = 1.0,
-                solver = dict(
-                    type = "ODESolver",
-                    args = dict(
-                        library="torchdyn",
-                    ),
+            device=device,
+            diffusion_model=dict(
+                device=device,
+                x_size=x_size,
+                alpha=1.0,
+                solver=dict(
+                    type="ODESolver",
+                    args=dict(library="torchdyn", ),
                 ),
-                path = dict(
-                    type = "linear_vp_sde",
-                    beta_0 = 0.1,
-                    beta_1 = 20.0,
+                path=dict(
+                    type="linear_vp_sde",
+                    beta_0=0.1,
+                    beta_1=20.0,
                 ),
-                model = dict(
-                    type = "velocity_function",
-                    args = dict(
-                        t_encoder = t_encoder,
-                        backbone = dict(
-                            type = "TemporalSpatialResidualNet",
-                            args = dict(
-                                hidden_sizes = [512, 256, 128],
-                                output_dim = x_size,
-                                t_dim = t_embedding_dim,
+                model=dict(
+                    type="velocity_function",
+                    args=dict(
+                        t_encoder=t_encoder,
+                        backbone=dict(
+                            type="TemporalSpatialResidualNet",
+                            args=dict(
+                                hidden_sizes=[512, 256, 128],
+                                output_dim=x_size,
+                                t_dim=t_embedding_dim,
                             ),
                         ),
                     ),
                 ),
             ),
-            parameter = dict(
-                training_loss_type = "flow_matching",
+            parameter=dict(
+                training_loss_type="flow_matching",
                 lr=5e-3,
                 data_num=10000,
                 weight_decay=1e-4,
@@ -117,77 +115,95 @@ def train_func():
                 video_save_path="./video",
                 device=device,
             ),
-    ))
-
+        )
+    )
 
     with wandb.init(
-        project="swiss_roll_diffusion_model",
-        config=origin_config,
+            project="swiss_roll_diffusion_model",
+            config=origin_config,
     ) as wandb_run:
         # config=merge_two_dicts_into_newone(EasyDict(wandb_run.config), origin_config)
         # wandb_run.config.update(config)
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        config=EasyDict(wandb.config)
+        config = EasyDict(wandb.config)
         run_name = f"{config.diffusion_model.path.type}-{config.diffusion_model.model.type}-{config.parameter.training_loss_type}--{timestamp}"
         wandb.run.name = run_name
         wandb.run.save()
-        diffusion_model = DiffusionModel(config=config.diffusion_model).to(config.diffusion_model.device)
+        diffusion_model = DiffusionModel(config=config.diffusion_model
+                                         ).to(config.diffusion_model.device)
         # diffusion_model = torch.compile(diffusion_model)
 
         # get data
-        data = make_swiss_roll(n_samples=config.parameter.data_num, noise=0.01)[0].astype(np.float32)[:,[0,2]]
+        data = make_swiss_roll(
+            n_samples=config.parameter.data_num, noise=0.01
+        )[0].astype(np.float32)[:, [0, 2]]
         # transform data
-        data[:,0] = data[:,0]/np.max(np.abs(data[:,0]))
-        data[:,1] = data[:,1]/np.max(np.abs(data[:,1]))
+        data[:, 0] = data[:, 0] / np.max(np.abs(data[:, 0]))
+        data[:, 1] = data[:, 1] / np.max(np.abs(data[:, 1]))
         data = (data - data.min()) / (data.max() - data.min())
         data = data * 10 - 5
 
-        
         optimizer = torch.optim.Adam(
-            diffusion_model.parameters(), 
+            diffusion_model.parameters(),
             lr=config.parameter.lr,
             weight_decay=config.parameter.weight_decay,
-            )
-
+        )
 
         last_iteration = -1
 
-        data_loader = torch.utils.data.DataLoader(data, batch_size=config.parameter.batch_size, shuffle=True)
+        data_loader = torch.utils.data.DataLoader(
+            data, batch_size=config.parameter.batch_size, shuffle=True
+        )
+
         def get_train_data(dataloader):
             while True:
                 yield from dataloader
+
         data_generator = get_train_data(data_loader)
 
-        gradient_sum=0.0
-        loss_sum=0.0
-        counter=0
-        iteration=0
+        gradient_sum = 0.0
+        loss_sum = 0.0
+        counter = 0
+        iteration = 0
 
         def plot2d(data):
 
-            plt.scatter(data[:,0], data[:,1])
+            plt.scatter(data[:, 0], data[:, 1])
             plt.show()
 
-        def render_video(data_list, video_save_path, iteration, fps=100, dpi=100):
+        def render_video(
+            data_list, video_save_path, iteration, fps=100, dpi=100
+        ):
             if not os.path.exists(video_save_path):
                 os.makedirs(video_save_path)
             fig = plt.figure(figsize=(6, 6))
             plt.xlim([-10, 10])
             plt.ylim([-10, 10])
             ims = []
-            colors=np.linspace(0,1,len(data_list))
+            colors = np.linspace(0, 1, len(data_list))
 
-            for i,data in enumerate(data_list):
+            for i, data in enumerate(data_list):
                 # image alpha frm 0 to 1
-                im = plt.scatter(data[:,0], data[:,1], s=1)
+                im = plt.scatter(data[:, 0], data[:, 1], s=1)
                 ims.append([im])
             ani = animation.ArtistAnimation(fig, ims, interval=0.1, blit=True)
-            ani.save(os.path.join(video_save_path, f"iteration_{iteration}.mp4"), fps=fps, dpi=dpi)
+            ani.save(
+                os.path.join(video_save_path, f"iteration_{iteration}.mp4"),
+                fps=fps,
+                dpi=dpi
+            )
             wandb_run.log(
                 data=dict(
-                    video=wandb.Video(os.path.join(video_save_path, f"iteration_{iteration}.mp4"), fps=fps, format="mp4"),
+                    video=wandb.Video(
+                        os.path.join(
+                            video_save_path, f"iteration_{iteration}.mp4"
+                        ),
+                        fps=fps,
+                        format="mp4"
+                    ),
                 ),
-                commit=False)
+                commit=False
+            )
             # clean up
             plt.close(fig)
             plt.clf()
@@ -200,67 +216,104 @@ def train_func():
                     model=model.state_dict(),
                     optimizer=optimizer.state_dict(),
                     iteration=iteration,
-                ),f=os.path.join(config.parameter.checkpoint_path, f"checkpoint_{iteration}.pt"))
+                ),
+                f=os.path.join(
+                    config.parameter.checkpoint_path,
+                    f"checkpoint_{iteration}.pt"
+                )
+            )
 
         history_iteration = [-1]
+
         def save_checkpoint_on_exit(model, optimizer, iterations):
+
             def exit_handler(signal, frame):
                 log.info("Saving checkpoint when exit...")
                 save_checkpoint(model, optimizer, iteration=iterations[-1])
                 log.info("Done.")
                 sys.exit(0)
+
             signal.signal(signal.SIGINT, exit_handler)
 
+        for iteration in track(range(config.parameter.iterations),
+                               description="Training"):
 
-        for iteration in track(range(config.parameter.iterations), description="Training"):
-            
             if iteration <= last_iteration:
                 continue
 
             if iteration > 0 and iteration % config.parameter.eval_freq == 0:
                 diffusion_model.eval()
-                t_span=torch.linspace(0.0, 1.0, 1000)
-                x_t = diffusion_model.sample_forward_process(t_span=t_span, batch_size=500).cpu().detach()
-                x_t=[x.squeeze(0) for x in torch.split(x_t, split_size_or_sections=1, dim=0)]
-                render_video(x_t, config.parameter.video_save_path, iteration, fps=100, dpi=100)
+                t_span = torch.linspace(0.0, 1.0, 1000)
+                x_t = diffusion_model.sample_forward_process(
+                    t_span=t_span, batch_size=500
+                ).cpu().detach()
+                x_t = [
+                    x.squeeze(0)
+                    for x in torch.split(x_t, split_size_or_sections=1, dim=0)
+                ]
+                render_video(
+                    x_t,
+                    config.parameter.video_save_path,
+                    iteration,
+                    fps=100,
+                    dpi=100
+                )
 
             batch_data = next(data_generator)
-            batch_data=batch_data.to(config.device)
+            batch_data = batch_data.to(config.device)
             # plot2d(batch_data.cpu().numpy())
             diffusion_model.train()
-            if config.parameter.training_loss_type=="flow_matching":
-                loss=diffusion_model.flow_matching_loss(batch_data)
-            elif config.parameter.training_loss_type=="score_matching":
-                loss=diffusion_model.score_matching_loss(batch_data)
+            if config.parameter.training_loss_type == "flow_matching":
+                loss = diffusion_model.flow_matching_loss(batch_data)
+            elif config.parameter.training_loss_type == "score_matching":
+                loss = diffusion_model.score_matching_loss(batch_data)
             else:
                 raise NotImplementedError("Unknown loss type")
             optimizer.zero_grad()
             loss.backward()
-            gradien_norm = torch.nn.utils.clip_grad_norm_(diffusion_model.parameters(), config.parameter.clip_grad_norm)
+            gradien_norm = torch.nn.utils.clip_grad_norm_(
+                diffusion_model.parameters(), config.parameter.clip_grad_norm
+            )
             optimizer.step()
-            gradient_sum+=gradien_norm.item()
-            loss_sum+=loss.item()
-            counter+=1
+            gradient_sum += gradien_norm.item()
+            loss_sum += loss.item()
+            counter += 1
 
-            log.info(f"iteration {iteration}, gradient {gradient_sum/counter}, loss {loss_sum/counter}")
+            log.info(
+                f"iteration {iteration}, gradient {gradient_sum/counter}, loss {loss_sum/counter}"
+            )
             history_iteration.append(iteration)
 
-            if iteration == config.parameter.iterations-1:
+            if iteration == config.parameter.iterations - 1:
                 diffusion_model.eval()
-                t_span=torch.linspace(0.0, 1.0, 1000)
-                x_t = diffusion_model.sample_forward_process(t_span=t_span, batch_size=500).cpu().detach()
-                x_t=[x.squeeze(0) for x in torch.split(x_t, split_size_or_sections=1, dim=0)]
-                render_video(x_t, config.parameter.video_save_path, iteration, fps=100, dpi=100)
+                t_span = torch.linspace(0.0, 1.0, 1000)
+                x_t = diffusion_model.sample_forward_process(
+                    t_span=t_span, batch_size=500
+                ).cpu().detach()
+                x_t = [
+                    x.squeeze(0)
+                    for x in torch.split(x_t, split_size_or_sections=1, dim=0)
+                ]
+                render_video(
+                    x_t,
+                    config.parameter.video_save_path,
+                    iteration,
+                    fps=100,
+                    dpi=100
+                )
 
             wandb_run.log(
                 data=dict(
                     iteration=iteration,
-                    gradient =gradient_sum/counter,
+                    gradient=gradient_sum / counter,
                     loss=loss.item(),
                 ),
-                commit=True)
+                commit=True
+            )
 
 
 if __name__ == '__main__':
-    sweep_id = wandb.sweep(sweep=sweep_config, project="swiss_roll_diffusion_model")
+    sweep_id = wandb.sweep(
+        sweep=sweep_config, project="swiss_roll_diffusion_model"
+    )
     wandb.agent(sweep_id, function=train_func)
