4ec7acbe66b847d8e100207aedf20e3cc522e37c
rongkunxue <rongkunxue@outlook.com>
2024-05-05 08:46:13 +0000

    polish code

--- grl/neural_network/encoders.py
+++ grl/neural_network/encoders.py
@@ -201,8 +201,23 @@ class ExponentialFourierProjectionTimeEncoder(nn.Module):
         t_emb = self.mlp(t_freq)
         return t_emb
 
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        device = x.device
+        half_dim = self.dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
+        emb = x[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
+
 ENCODERS={
     "GaussianFourierProjectionTimeEncoder".lower(): GaussianFourierProjectionTimeEncoder,
     "GaussianFourierProjectionEncoder".lower(): GaussianFourierProjectionEncoder,
     "ExponentialFourierProjectionTimeEncoder".lower(): ExponentialFourierProjectionTimeEncoder,
+    "SinusoidalPosEmb".lower(): SinusoidalPosEmb
 }
