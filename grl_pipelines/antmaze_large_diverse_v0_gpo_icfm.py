25ea08f51fc495b26961ba354f68160c1c8e0f65
zjowowen <zjowowen@outlook.com>
2024-05-12 04:50:13 +0800

    add antmaze pipelines

---
+++ grl_pipelines/antmaze_large_diverse_v0_gpo_icfm.py
@@ -0,0 +1,39 @@
+import gym
+import d4rl
+
+from grl.algorithms.gpo import GPOAlgorithm
+from grl.utils.log import log
+from grl_pipelines.configurations.antmaze_large_diverse_v0_gpo_icfm import (
+    config,
+)
+
+
+def gpo_pipeline(config):
+
+    gpo = GPOAlgorithm(config)
+
+    # ---------------------------------------
+    # Customized train code ↓
+    # ---------------------------------------
+    gpo.train()
+    # ---------------------------------------
+    # Customized train code ↑
+    # ---------------------------------------
+
+    # ---------------------------------------
+    # Customized deploy code ↓
+    # ---------------------------------------
+    agent = gpo.deploy()
+    env = gym.make(config.deploy.env.env_id)
+    observation = env.reset()
+    for _ in range(config.deploy.num_deploy_steps):
+        env.render()
+        observation, reward, done, _ = env.step(agent.act(observation))
+    # ---------------------------------------
+    # Customized deploy code ↑
+    # ---------------------------------------
+
+
+if __name__ == "__main__":
+    log.info("config: \n{}".format(config))
+    gpo_pipeline(config)
