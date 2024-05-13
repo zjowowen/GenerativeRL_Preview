8be21d4886d365a016c32e74b51abe79bbc97dd3
rongkunxue <rongkunxue@outlook.com>
2024-05-13 02:50:28 +0000

    add gpg

---
+++ grl_pipelines/ddp/configurations/lunarlander_continuous_gpg_icfm.py
@@ -0,0 +1,148 @@
+import torch
+from easydict import EasyDict
+
+
+def make_config(device, batch_size_ratio=1):
+
+    action_size = 2
+    state_size = 8
+
+    t_embedding_dim = 32
+    t_encoder = dict(
+        type="GaussianFourierProjectionTimeEncoder",
+        args=dict(
+            embed_dim=t_embedding_dim,
+            scale=30.0,
+        ),
+    )
+    algorithm_type = "GPG"
+    solver_type = "ODESolver"
+    model_type = "IndependentConditionalFlowModel"
+    project_name = "LunarLanderContinuous-v2-GPG-IndependentConditionalFlowModel-DDP"
+
+    model = dict(
+        device=device,
+        x_size=action_size,
+        solver=dict(
+            type="ODESolver",
+            args=dict(
+                library="torchdiffeq_adjoint",
+            ),
+        ),
+        path=dict(
+            sigma=0.1,
+        ),
+        model=dict(
+            type="velocity_function",
+            args=dict(
+                t_encoder=t_encoder,
+                backbone=dict(
+                    type="TemporalSpatialResidualNet",
+                    args=dict(
+                        hidden_sizes=[512, 256, 128],
+                        output_dim=action_size,
+                        t_dim=t_embedding_dim,
+                        condition_dim=state_size,
+                        condition_hidden_dim=32,
+                        t_condition_hidden_dim=128,
+                    ),
+                ),
+            ),
+        ),
+    )
+
+    return EasyDict(
+        train=dict(
+            project=project_name,
+            device=device,
+            simulator=dict(
+                type="GymEnvSimulator",
+                args=dict(
+                    env_id="LunarLanderContinuous-v2",
+                ),
+            ),
+            # dataset=dict(
+            #     type="QGPOCustomizedDataset",
+            #     args=dict(
+            #         env_id="LunarLanderContinuous-v2",
+            #         device=device,
+            #         numpy_data_path = "./data.npz",
+            #     ),
+            # ),
+            model=dict(
+                GPPolicy=dict(
+                    device=device,
+                    model_type=model_type,
+                    model=model,
+                    critic=dict(
+                        device=device,
+                        q_alpha=1.0,
+                        DoubleQNetwork=dict(
+                            backbone=dict(
+                                type="ConcatenateMLP",
+                                args=dict(
+                                    hidden_sizes=[action_size + state_size, 256, 256],
+                                    output_size=1,
+                                    activation="relu",
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+                GuidedPolicy=dict(
+                    model_type=model_type,
+                    model=model,
+                ),
+            ),
+            parameter=dict(
+                algorithm_type=algorithm_type,
+                behaviour_policy=dict(
+                    batch_size=2048 * batch_size_ratio,
+                    learning_rate=1e-4,
+                    epochs=1000,
+                    # new add below
+                    grad_norm_clip=10.0,
+                    lr_decy=False,
+                ),
+                sample_per_state=16,
+                fake_data_t_span=None if solver_type == "DPMSolver" else 32,
+                critic=dict(
+                    batch_size=2048 * batch_size_ratio,
+                    epochs=1000,
+                    learning_rate=3e-4,
+                    discount_factor=0.99,
+                    update_momentum=0.005,
+                    # new add below
+                    grad_norm_clip=10.0,
+                    lr_decy=False,
+                ),
+                guided_policy=dict(
+                    batch_size=2048 * batch_size_ratio,
+                    epochs=1000,
+                    learning_rate=1e-4,
+                    # new add below
+                    copy_frome_basemodel=True,
+                    grad_norm_clip=10.0,
+                    lr_decy=True,
+                    loss_type="double_minibatch_loss",
+                ),
+                evaluation=dict(
+                    eval=True,
+                    evaluation_behavior_policy_interval=20,
+                    evaluation_guided_policy_interval=20,
+                    guidance_scale=[0.0, 1.0, 2.0],
+                ),
+                checkpoint_path=f"./{project_name}/checkpoint",
+                checkpoint_freq=100,
+            ),
+        ),
+        deploy=dict(
+            device=device,
+            env=dict(
+                env_id="LunarLanderContinuous-v2",
+                seed=0,
+            ),
+            num_deploy_steps=1000,
+            t_span=None if solver_type == "DPMSolver" else 32,
+        ),
+    )
---
+++ grl_pipelines/ddp/lunarlander_continuous_gpg_gvp_torchrun.py
@@ -0,0 +1,51 @@
+# torchrun --nproc_per_node=8 this file
+import gym
+import torch
+
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+from grl.algorithms.ddp.gp import GPAlgorithm
+from grl.datasets import GPOCustomizedDataset
+from grl_pipelines.ddp.configurations.lunarlander_continuous_gpg_gvp import (
+    make_config,
+)
+
+
+def gp_pipeline(config):
+
+    gp = GPAlgorithm(
+        config,
+        dataset=GPOCustomizedDataset(
+            numpy_data_path="./data.npz", device=config.train.device
+        ),
+    )
+
+    # ---------------------------------------
+    # Customized train code ↓
+    # ---------------------------------------
+    gp.train()
+    # ---------------------------------------
+    # Customized train code ↑
+    # ---------------------------------------
+
+    # ---------------------------------------
+    # Customized deploy code ↓
+    # ---------------------------------------
+    if torch.distributed.get_rank() == 0:
+        agent = gp.deploy()
+        env = gym.make(config.deploy.env.env_id)
+        observation = env.reset()
+        for _ in range(config.deploy.num_deploy_steps):
+            env.render()
+            observation, reward, done, _ = env.step(agent.act(observation))
+    # ---------------------------------------
+    # Customized deploy code ↑
+    # ---------------------------------------
+
+
+if __name__ == "__main__":
+    torch.distributed.init_process_group("nccl")
+    device = torch.distributed.get_rank() % torch.cuda.device_count()
+    torch.cuda.set_device(device)
+    config = make_config(device=device, batch_size_ratio=torch.cuda.device_count())
+    gp_pipeline(config)
