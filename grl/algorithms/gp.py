97d4e4d61c572ddbcd6463d78db8c81b4f64ed7d
rongkunxue <rongkunxue@outlook.com>
2024-05-14 07:12:05 +0000

    polish gp

--- grl/algorithms/gp.py
+++ grl/algorithms/gp.py
@@ -653,6 +653,8 @@ class GPAlgorithm:
             ),
             **config.wandb if hasattr(config, "wandb") else {},
         ) as wandb_run:
+            if not hasattr(config.model.GPPolicy, "model_loss_type"):
+                config.model.GPPolicy["model_loss_type"] = "flow_matching"
             if config.parameter.algorithm_type == "GPO":
                 run_name = f"{config.model.GPPolicy.model_loss_type}-{config.model.GPPolicy.model.model.type}-{seed_value}"
                 wandb.run.name = run_name
