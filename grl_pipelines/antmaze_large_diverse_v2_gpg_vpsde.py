2d04ba3f46e93594cdf89eb1862f94e8af2d7bd9
rongkunxue <rongkunxue@outlook.com>
2024-05-14 03:46:45 +0000

    poilsh gpg

---
+++ grl_pipelines/antmaze_large_diverse_v2_gpg_vpsde.py
@@ -0,0 +1,39 @@
+import gym
+
+from grl.algorithms.gp import GPAlgorithm
+from grl.datasets import GPOCustomizedDataset
+from grl.utils.log import log
+from grl_pipelines.configurations.antmaze_large_diverse_v0_gpg_vpsde import (
+    config,
+)
+
+
+def gp_pipeline(config):
+
+    gp = GPAlgorithm(config)
+
+    # ---------------------------------------
+    # Customized train code ↓
+    # ---------------------------------------
+    gp.train()
+    # ---------------------------------------
+    # Customized train code ↑
+    # ---------------------------------------
+
+    # ---------------------------------------
+    # Customized deploy code ↓
+    # ---------------------------------------
+    agent = gp.deploy()
+    env = gym.make(config.deploy.env.env_id)
+    observation = env.reset()
+    for _ in range(config.deploy.num_deploy_steps):
+        env.render()
+        observation, reward, done, _ = env.step(agent.act(observation))
+    # ---------------------------------------
+    # Customized deploy code ↑
+    # ---------------------------------------
+
+
+if __name__ == "__main__":
+    log.info("config: \n{}".format(config))
+    gp_pipeline(config)
---
+++ grl_pipelines/lunarlander_continuous_v2_gpg_gvp.py
@@ -0,0 +1,44 @@
+import gym
+
+from grl.algorithms.gp import GPAlgorithm
+from grl.datasets import GPOCustomizedDataset
+from grl.utils.log import log
+from grl_pipelines.configurations.lunarlander_continuous_v2_gpg_gvp import (
+    config,
+)
+
+
+def gp_pipeline(config):
+
+    gp = GPAlgorithm(
+        config,
+        dataset=GPOCustomizedDataset(
+            numpy_data_path="./data.npz", device=config.train.device
+        ),
+    )
+
+    # ---------------------------------------
+    # Customized train code ↓
+    # ---------------------------------------
+    gp.train()
+    # ---------------------------------------
+    # Customized train code ↑
+    # ---------------------------------------
+
+    # ---------------------------------------
+    # Customized deploy code ↓
+    # ---------------------------------------
+    agent = gp.deploy()
+    env = gym.make(config.deploy.env.env_id)
+    observation = env.reset()
+    for _ in range(config.deploy.num_deploy_steps):
+        env.render()
+        observation, reward, done, _ = env.step(agent.act(observation))
+    # ---------------------------------------
+    # Customized deploy code ↑
+    # ---------------------------------------
+
+
+if __name__ == "__main__":
+    log.info("config: \n{}".format(config))
+    gp_pipeline(config)
