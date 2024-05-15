76081b71458b4f34e5180ef42f94142a4aa40466
rongkunxue <rongkunxue@outlook.com>
2024-05-15 07:19:08 +0000

    polish equl

--- grl/algorithms/gp.py
+++ grl/algorithms/gp.py
@@ -1109,7 +1109,7 @@ class GPAlgorithm:
             # guided policy training code ↓
             # ---------------------------------------
 
-            if config.parameter.guided_policy.copy_from_basemodel:
+            if  hasattr(config.parameter.guided_policy,"copy_from_basemodel") and config.parameter.guided_policy.copy_from_basemodel:
                 self.model["GPPolicy"].guided_model.model.load_state_dict(
                     self.model["GPPolicy"].base_model.model.state_dict()
                 )
