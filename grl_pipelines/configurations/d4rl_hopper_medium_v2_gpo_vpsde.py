fa0684f9f4077f402f8f7157653d9a00d7380cdd
zjowowen <zjowowen@outlook.com>
2024-05-16 03:04:27 +0800

    polish config

--- grl_pipelines/configurations/d4rl_hopper_medium_v2_gpo_vpsde.py
+++ grl_pipelines/configurations/d4rl_hopper_medium_v2_gpo_vpsde.py
@@ -91,7 +91,7 @@ config = EasyDict(
             ),
         ),
         model=dict(
-            GPOPolicy=dict(
+            GPPolicy=dict(
                 device=device,
                 model_type=model_type,
                 model_loss_type=model_loss_type,
